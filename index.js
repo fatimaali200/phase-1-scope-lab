19033404fbb7faa4ce98b90937f9b8dd337b6af1
Fatima Ali <fofoalmali@gmail.com>
2023-03-28 15:13:46 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName='bob';
+//-1-
+function upperCaseCustomerName(){
+    customerName= customerName.toUpperCase();
+}
+//-2-
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+//-3-
+function overwriteBestCustomer(){
+    bestCustomer='maybe bob'
+}
+
+//-4-
+const leastFavoriteCustomer='fatima';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer='not fatima';
+}
+  
